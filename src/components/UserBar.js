299e0d775adc09c31482e96fe19cbb443fddc0f7
Joaquin Viera <jviera@asapdevelopers.com>
2020-07-14 11:14:45 -0300

    usebar changes

--- src/components/UserBar.js
+++ src/components/UserBar.js
@@ -3,13 +3,6 @@ import React from 'react';
 import { View, Text, Image, TouchableOpacity } from 'react-native';
 import { humanizeTimestamp } from '../utils';
 
-import Avatar from './Avatar';
-import FollowButton from './FollowButton';
-import type { StyleSheetLike } from '../types';
-import { buildStylesheet } from '../styles';
-import { withTranslationContext } from '../Context';
-import type { Streami18Ctx } from '../Context';
-
 type Props = {|
   username: ?string,
   avatar?: string,
@@ -27,24 +20,42 @@ type Props = {|
  * A compact horizontal user information box (it is used as activities' header)
  * @example ./examples/UserBar.md
  */
-const UserBar = withTranslationContext(
+const UserBar =
   ({
     username,
+    location,
     subtitle,
     avatar,
     follow,
     onPressAvatar,
     icon,
     tDateTimeParser,
+    addPerson,
+    font
     ...props
-  }: Props) => {
+  }) => {
     username = username || 'Unknown';
+    location = location || 'Unknown';
     let time = props.time;
     if (time === undefined && props.timestamp != null) {
       time = humanizeTimestamp(props.timestamp, tDateTimeParser);
     }
 
     const styles = buildStylesheet('userBar', props.styles);
+
+    const customStyles = StyleSheet.create({
+      usernameLocation: {
+        flexDirection: "row"
+      },
+      location: {
+        fontFamily: "Roboto",
+        fontStyle: "normal",
+        fontSize: 18,
+        lineHeight: 22,
+        color: 'gray',
+      }
+
+    });
 
     return (
       <View style={styles.container}>
@@ -62,7 +73,11 @@ const UserBar = withTranslationContext(
         ) : null}
 
         <View style={styles.content}>
-          <Text style={styles.username}>{username}</Text>
+          <View style={customStyles.usernameLocation}>
+            <Text style={styles.username}>{username}{" "}</Text>
+            <Text style={customStyles.location}>{location}</Text>
+          </View>
+
           <View style={{ flexDirection: 'row' }}>
             {icon !== undefined ? (
               <Image
@@ -73,19 +88,21 @@ const UserBar = withTranslationContext(
             {subtitle && <Text style={styles.subtitle}>{subtitle}</Text>}
           </View>
         </View>
-        {time && (
+        {/* {time && (
           <View>
             <Text style={styles.time}>{time}</Text>
           </View>
-        )}
-        {follow && (
+        )} */}
+        <View>
+          <Image source={addPerson} testID="coin-test" />
+        </View>
+        {/* {follow && (
           <View>
             <FollowButton followed />
           </View>
-        )}
+        )} */}
       </View>
     );
-  },
-);
+  }
 
 export default UserBar;
