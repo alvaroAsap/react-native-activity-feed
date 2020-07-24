317f9e5f1ce6dc8da0056250f1da985eb67acb71
Joaquin Viera <jviera@asapdevelopers.com>
2020-07-24 12:40:01 -0300

    location flex direction column

--- src/components/UserBar.js
+++ src/components/UserBar.js
@@ -52,7 +52,7 @@ const UserBar = withTranslationContext(
 
     const customStyles = StyleSheet.create({
       usernameLocation: {
-        flexDirection: "row"
+        flexDirection: "column"
       },
       location: {
         fontFamily: "Roboto",
